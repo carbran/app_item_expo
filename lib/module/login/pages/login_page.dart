839e00dbb881aaa5d7af1d0d5f0914e784476476
Fernando Carlos Brandão Filho <brandao.fernando96@gmail.com>
2024-12-02 23:31:03 -0300

    :wrench: chore: configuracoes do app, dependencias, adicionado icone do app

--- lib/module/login/pages/login_page.dart
+++ lib/module/login/pages/login_page.dart
@@ -19,61 +19,103 @@ class LoginPage extends GetView<LoginController> {
       body: Center(
         child: SizedBox(
           width: Get.width * 0.9,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            Padding(
-                padding: _padding,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: _padding,
-                      child: SvgPicture.asset(
-                        SvgGallery.itemExpoLogo,
-                        width: Get.width * 0.7,
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: _padding,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: _padding,
+                        child: SvgPicture.asset(
+                          SvgGallery.itemExpoLogo,
+                          width: Get.width * 0.7,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: _padding,
-                      child: TextFormField(
-                        controller: TextEditingController(),
-                        decoration: const InputDecoration(labelText: 'E-mail'),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) => emailValidator!(value!),
-                      ),
-                    ),
-                    Obx(
-                      () {
-                        return Padding(
-                          padding: _padding,
-                          child: TextFormField(
-                            controller: TextEditingController(),
-                            decoration:
-                                const InputDecoration(labelText: 'Senha'),
-                            obscureText: controller.hidePass.value,
+                      Padding(
+                        padding: _padding,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: ItemExpoColors.white,
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        );
-                      },
-                    ),
-                    Padding(
-                      padding: _padding,
-                      child: Obx(
-                        () => !controller.waiting.value
-                            ? ElevatedButton(
-                                onPressed: controller.login,
-                                child: const Text('Entrar'))
-                            : Lottie.asset(
-                                AnimGallery.loader,
-                                width: 60,
-                                height: 52,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            ),
+                            child: TextFormField(
+                              controller: TextEditingController(),
+                              decoration: const InputDecoration(
+                                labelText: 'E-mail',
+                                border: InputBorder.none,
                               ),
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value) => emailValidator!(value!),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      Obx(
+                        () {
+                          return Padding(
+                            padding: _padding,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: ItemExpoColors.white,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16.0,
+                                ),
+                                child: TextFormField(
+                                  controller: TextEditingController(),
+                                  decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    border: InputBorder.none,
+                                    suffixIcon: TextButton(
+                                      onPressed: () {
+                                        controller.hidePass.value =
+                                            !controller.hidePass.value;
+                                      },
+                                      child: Icon(
+                                        !controller.hidePass.value
+                                            ? Icons.visibility_off_outlined
+                                            : Icons.visibility_outlined,
+                                      ),
+                                    ),
+                                  ),
+                                  obscureText: controller.hidePass.value,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      Padding(
+                        padding: _padding,
+                        child: Obx(
+                          () => !controller.waiting.value
+                              ? ElevatedButton(
+                                  onPressed: controller.login,
+                                  child: const Text('Entrar'))
+                              : Lottie.asset(
+                                  AnimGallery.loader,
+                                  width: 60,
+                                  height: 52,
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
